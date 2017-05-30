3671d9053a271cff0b1dc607ac810edefb4e0044
Nicolas De Giacomo <nicolas.de.giacomo@gmail.com>
2017-05-30 01:46:35 -0300

    TDA Counting Filters —> Inicial primitive description

---
+++ CountingFilters.h
@@ -0,0 +1,6 @@
+//Primitivas:
+//Agregar filtro -> Hashea el filtro (string) x veces (x es la cantidad de funciones hash distintas)
+// y setea el valor de un array (un array para cada x) en cero.
+//Aumentar filtro -> Agrega el filtro si no existe -> Aumenta en todos los x arrays el valor del filtro en 1
+//Ver valor del filtro -> Devuelve el valor minimo de los x arrays para el filtro
+//Filtro existe -> Devuelve si existe el filtro
