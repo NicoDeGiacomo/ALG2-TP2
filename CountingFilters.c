807620f454d52a035061f730e5675dfbaee283bf
Nicolas De Giacomo <nicolas.de.giacomo@gmail.com>
2017-06-06 01:15:18 -0300

    TODO added

--- CountingFilters.c
+++ CountingFilters.c
@@ -28,6 +28,7 @@ size_t prime_hash(const char *key, size_t max_size);
 /*PRIMITIVAS*/
 
 counting_filter_t* counting_filter_crear(size_t size){
+    //TODO: ENCONTRAR EL PRIMO INMEDIATO SIGUIENTE Y CREAR CON ESE TAMAÑO PARA EVITAR COLISIONES
     counting_filter_t* counting_filter = malloc(sizeof(counting_filter_t));
     if(!counting_filter)
         return NULL;
