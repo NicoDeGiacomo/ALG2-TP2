52fd79f1048e6d08f2d1592d77832d6e770f855c
Nicolas De Giacomo <nicolas.de.giacomo@gmail.com>
2017-06-09 12:52:36 -0300

    utils.h added

---
+++ utils.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char** split(const char* str, char sep);
+
+void concat(char s1[], size_t len1, char s2[]);
+
+char* join(char** strv, char sep);
+
+void free_strv(char *strv[]);
+
+//Obtiene n lineas del archivo (para si llegó al eof).
+//Si n es NULL lee el archivo completo.
+//Devuelve un array (dinamically allocated) terminado en NULL con las lineas (dynamically allocated) en forma de string.
+char** obtener_lineas(FILE* file, size_t n);
